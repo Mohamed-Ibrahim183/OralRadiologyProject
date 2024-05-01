e8e9978c2193bc3e25c89575db7ed2bfec62bebf
Mohamed-Ibrahim111 <125296753+Mohamed-Ibrahim183@users.noreply.github.com>
2024-05-01 14:58:07 -0700

    Fix student and Professor DB

--- src/Pages/StudentDashBoard/StudentDB.js
+++ src/Pages/StudentDashBoard/StudentDB.js
@@ -4,9 +4,8 @@ import Navbar from "../../Components/Navbar/Navbar";
 import AssignmentCard from "./AssignmentCard";
 import Chart from "./Chart";
 import Calendarr from "./Calender";
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
-
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 const StudentDB = () => {
   return (
@@ -14,61 +13,63 @@ const StudentDB = () => {
       <Navbar />
       <div className="fullPage">
         <div className="upppperr">
-            <div className="container AssignmentSection">
-              <div className="BBBBBB">
-                  <h2 className="sectionTitle">My Assignments</h2>
-                  <h5 className="seeall">See all →</h5>
-              </div>
-              <div className="CARDSASSIGNMENT">
-                <AssignmentCard
-                  name="Assignment 1"
-                  state="Good"
-                  info="Topic Name, April 30, 2024, 1:00 pm"
-                  grade="60"
-                  col="#0082e6"
-                ></AssignmentCard>
-                <AssignmentCard
-                  name="Assignment 1"
-                  state="Fail"
-                  info="Topic Name, April 30, 2024, 1:00 pm"
-                  grade="49"
-                  col="red"
-                  // col="#39444C"
-                ></AssignmentCard>
-                <AssignmentCard
-                  name="Assignment 1"
-                  state="not Graded"
-                  info="Topic Name, April 30, 2024, 1:00 pm"
-                  grade="--"
-                  col="#FFD269"
-                ></AssignmentCard>
-                <AssignmentCard
-                  name="Assignment 1"
-                  state="not Graded"
-                  info="Topic Name, April 30, 2024, 1:00 pm"
-                  grade="--"
-                  col="green"
-                ></AssignmentCard>
-              </div>
+          <div className="container AssignmentSection">
+            <div className="BBBBBB">
+              <h2 className="sectionTitle">My Assignments</h2>
+              <h5 className="seeall">See all →</h5>
             </div>
-            <div className="USERPROF">
-              <div className="TEXT">
+            <div className="CARDSASSIGNMENT">
+              <AssignmentCard
+                name="Assignment 1"
+                state="Good"
+                info="Topic Name, April 30, 2024, 1:00 pm"
+                grade="60"
+                col="#0082e6"
+              ></AssignmentCard>
+              <AssignmentCard
+                name="Assignment 1"
+                state="Fail"
+                info="Topic Name, April 30, 2024, 1:00 pm"
+                grade="49"
+                col="red"
+                // col="#39444C"
+              ></AssignmentCard>
+              <AssignmentCard
+                name="Assignment 1"
+                state="not Graded"
+                info="Topic Name, April 30, 2024, 1:00 pm"
+                grade="--"
+                col="#FFD269"
+              ></AssignmentCard>
+              <AssignmentCard
+                name="Assignment 1"
+                state="not Graded"
+                info="Topic Name, April 30, 2024, 1:00 pm"
+                grade="--"
+                col="green"
+              ></AssignmentCard>
+            </div>
+          </div>
+          <div className="USERPROF">
+            <div className="TEXT">
               <h4>Welcome Back</h4>
               <h2>Abdelmonem Hatem</h2>
               <p>Welcome to our Oral Radiology system</p>
-              </div>
-              <button type="button" className="">Go To Profile</button>
             </div>
+            <button type="button" className="">
+              Go To Profile
+            </button>
+          </div>
         </div>
         <div className="Loowwerr">
-            <div className="cc1" >
+          <div className="cc1">
             <Chart className="chart1" />
-            </div>
-            <div className="Calenderrrr">
+          </div>
+          <div className="Calenderrrr">
             {/* <Calendarr year={2024} month={4} />  */}
             <h2>Calendar</h2>
-            <Calendar ></Calendar>
-            </div>
+            <Calendar></Calendar>
+          </div>
         </div>
       </div>
     </>
