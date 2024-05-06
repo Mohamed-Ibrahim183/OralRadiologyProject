26ee78a713dc6c53a2260cca84d4e9ee6cc6336b
Mohamed-Ibrahim111 <125296753+Mohamed-Ibrahim183@users.noreply.github.com>
2024-05-06 13:52:18 -0700

    New Feature in Users Page

--- src/Pages/Users/Users.js
+++ src/Pages/Users/Users.js
@@ -5,6 +5,8 @@ import axios from "axios";
 
 const Users = () => {
   const [view, setView] = useState([]);
+  const [Type, setType] = useState("Student");
+  const [data, setData] = useState([]);
 
   useEffect(() => {
     document.body.classList.add("TableBody");
@@ -14,32 +16,51 @@ const Users = () => {
     return () => document.body.classList.remove("TableBody");
   }, []);
 
+  function FrontView(dataContent) {
+    const content = dataContent.map((ele) => (
+      <tr key={ele.Id}>
+        <td>{ele.Id}</td>
+        <td>{ele.MSAId}</td>
+        <td>{ele.Type}</td>
+        <td>{ele.Email}</td>
+        <td className="select">
+          <button className="button edit">Edit</button>
+          <button className="button del">Delete</button>
+        </td>
+      </tr>
+    ));
+    setView(content);
+  }
+
   function FetchFirst() {
     const url = "http://localhost/Projects/OralRadiology/userLogic.php/Users";
-    axios
-      .get(url)
-      .then((res) => {
-        console.log(res.data);
-        const content = res.data.map((ele) => (
-          <tr key={ele.Id}>
-            <td>{ele.Id}</td>
-            <td>{ele.MSAId}</td>
-            <td>{ele.Type}</td>
-            <td>{ele.Email}</td>
-            <td className="select">
-              <button className="button edit">Edit</button>
-              <button className="button del">Delete</button>
-            </td>
-          </tr>
-        ));
-        setView(content);
-      })
-      .catch((error) => console.log(error));
+    axios.get(url).then((res) => {
+      console.log(res.data);
+      if (res.data) FrontView(res.data);
+    });
+    // .catch((error) => console.log(error));
   }
+
+  function handleType(e) {
+    setType(e.target.value);
+  }
+  useEffect(() => {
+    // console.log(Type);
+    const url =
+      "http://localhost/Projects/OralRadiology/userLogic.php/Users/" + Type;
+    axios.get(url).then((res) => {
+      FrontView(res.data);
+    });
+  }, [Type]);
 
   return (
     <>
       <Navbar />
+      <select name="Type" id="Type" onChange={handleType}>
+        <option value="Student">Student</option>
+        <option value="Professor">Professor</option>
+        <option value="Admin">Admin</option>
+      </select>
       <div className="Table">
         <h1>Users</h1>
         <main>
