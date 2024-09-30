ebec5bf7c477df95061f22ee90e163744dbf1028
Mohamed-Ibrahim111 <125296753+Mohamed-Ibrahim183@users.noreply.github.com>
2024-09-29 23:09:59 -0700

    "Modified conditional checks and error handling for file uploads in AssignmentPage2 component" #Mohamed

--- src/Pages/assignment_page2/assignmentPage2.js
+++ src/Pages/assignment_page2/assignmentPage2.js
@@ -205,12 +205,18 @@ const AssignmentPage2 = () => {
       alert("Please upload files for all categories.");
       return;
     }
-    if (categories.length > uploadedFiles.length) {
-      toast.error(
-        "You Must Upload Film For Each Category to Finish upload the Requirement"
-      );
+    // if (categories.length > uploadedFiles.length) {
+    //   toast.error(
+    //     "You Must Upload Film For Each Category to Finish upload the Requirement"
+    //   );
+    //   return;
+    // }
+    if (
+      !window.confirm(
+        "Do You Want to submit the assignment not completed the categories"
+      )
+    )
       return;
-    }
 
     try {
       const { msg: lastSubmission } = await makeNewSubmission({
